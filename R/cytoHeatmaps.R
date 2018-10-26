6e05ae866af37cd5a8824d00a9f6b33f589a50d7
KoenAStam <k.a.stam@hotmail.com>
2018-10-26 10:33:21 +0200

    set vapply instead of sapply

--- R/cytoHeatmaps.R
+++ R/cytoHeatmaps.R
@@ -134,7 +134,7 @@ cytoHeatmaps <- function(cfList, group, legend = FALSE){
   ## Extract ggplot legend
   g_legend<-function(a.gplot){
     tmp <- ggplot_gtable(ggplot_build(a.gplot))
-    leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+    leg <- which(vapply(tmp$grobs, function(x) x$name) == "guide-box")
     legend <- tmp$grobs[[leg]]$grobs[[1]]
     legend
   }
