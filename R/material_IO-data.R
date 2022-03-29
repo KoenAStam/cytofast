249f9b4ba96739213c002271eada394c2e638c1b
KoenAStam <koen.a.stam@gmail.com>
2022-03-29 10:30:09 +0200

    forgot to add

---
+++ R/material_IO-data.R
@@ -0,0 +1,30 @@
+#' Color palette from material.io
+#'
+#' A dataset containing hexcodes for several colors
+#'
+#' @format A data frame with 14 observations and 19 variables:
+#' \describe{
+#'  \item{red}{shades of red}
+#'  \item{pink}{shades of pink}
+#'  \item{purple}{shades of purple}
+#'  \item{deep.purple}{shades of deep purple}
+#'  \item{indigo}{shades of indigo}
+#'  \item{blue}{shades of blue}
+#'  \item{light.blue}{shades of light blue}
+#'  \item{cyan}{shades of cyan}
+#'  \item{teal}{shades of teal}
+#'  \item{green}{shades of green}
+#'  \item{light.green}{shades of light green}
+#'  \item{lime}{shades of lime}
+#'  \item{yellow}{shades of yellow}
+#'  \item{amber}{shades of amber}
+#'  \item{orange}{shades of orange}
+#'  \item{deep.orange}{shades of deep orange}
+#'  \item{brown}{shades of brown}
+#'  \item{gray}{shades of gray}
+#'  \item{blue.gray}{shades of blue.gray}
+#'  }
+#'
+#'
+#' @source \url{https://material.io/resources/color/}
+"material_io"
